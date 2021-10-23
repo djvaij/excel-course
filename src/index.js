bd0fb17d2b303166dbbd99cb02d31a112349dbf6
djvaij <djvaij@gmail.com>
2021-10-23 10:31:13 +0300

    Finish css layout

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-import './scss/index.scss'
+import './scss/index.scss';
 
-console.log('Working')
+console.log('Working');
+
+const cells = document.querySelectorAll('.cell');
+let selectedElement = document.querySelectorAll('.cell.selected')[0];
+
+cells.forEach((el) => {
+  el.addEventListener('click', (e) => {
+    selectedElement.classList.remove('selected');
+    e.currentTarget.classList.add('selected');
+    selectedElement = e.currentTarget;
+  });
+});
